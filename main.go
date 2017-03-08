a944ad7cc5f58f27d5f5303efac4c41936ff2276
Josh Luna <ohbicboi@live.com>
2017-03-07 23:55:27 -0800

    made changes to trim new line after inputting string

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
 
 func main() {
+	fname_reader := bufio.NewReader(os.Stdin)
+	fmt.Print("First Name: ")
+	fname, _ := fname_reader.ReadString('\n')
+	fname = strings.TrimSpace(fname)
+
+	lname_reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Last Name: ")
+	lname, _ := lname_reader.ReadString('\n')
+	lname = strings.TrimSpace(lname)
+
+	fmt.Println(fname, lname)
 
 }
